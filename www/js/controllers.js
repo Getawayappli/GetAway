d85024c142b19555b7e3d29fc8f9feeccfe8ee9a
khaoulaari <khawla.ari@gmail.com>
2016-05-13 14:39:08 +0200

    commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,8 +1,45 @@
 angular.module('app.controllers', [])
 
 
-.controller('loginCtrl', function($scope) {
+.controller('loginCtrl', function($scope, $ionicPopup, $timeout) {
 
+ // Triggered on a button click, or some other target
+ $scope.showPopup = function() {
+   $scope.data = {}
+
+   // An elaborate, custom popup
+   var myPopup = $ionicPopup.show({
+     template: '<input type="text" placeholder="Identifiant/Mail">'
+     			+ '</br>'
+     			+'<input type="password" placeholder="Mot de passe " ng-model="data.wifi">'
+     			+'<a href="#">Pas de compte? Inscrivez-Vous </a> ',
+     title: 'Connexion',
+     subTitle: 'veuillez entrer vos informations pour se connecter',
+     scope: $scope,
+     buttons: [
+       { text: 'Cancel' },
+       {
+         text: '<b>Login</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+           if (!$scope.data.wifi) {
+             //don't allow the user to close unless he enters wifi password
+             e.preventDefault();
+           } else {
+             return $scope.data.wifi;
+           }
+         }
+       },
+     ]
+   });
+   myPopup.then(function(res) {
+     console.log('Tapped!', res);
+   });
+   $timeout(function() {
+     // myPopup.close(); //close the popup after 3 seconds for some reason
+   }, 3000);
+  };
+  
 })
 
 .controller('inscriptionCtrl', function($scope) {
