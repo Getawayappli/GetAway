08c0f9c3d92d27c4a80b50bc83e867c0542d78ca
khaoulaari <khawla.ari@gmail.com>
2016-06-08 12:11:39 +0200

    commit

--- www/js/services.js
+++ www/js/services.js
@@ -52,6 +52,23 @@ angular.module('app.services', ['firebase'])
     }
 })
 
+
+.service('UserService', function() {
+  // For the purpose of this example I will store user data on ionic local storage but you should save it on a database
+  var setUser = function(user_data) {
+    window.localStorage.starter_facebook_user = JSON.stringify(user_data);
+  };
+
+  var getUser = function(){
+    return JSON.parse(window.localStorage.starter_facebook_user || '{}');
+  };
+
+  return {
+    getUser: getUser,
+    setUser: setUser
+  };
+})
+
   .factory("Auth", function($firebaseAuth) {
   var usersRef = new Firebase('https://test-e06ab.firebaseio.com/user');
   return $firebaseAuth(usersRef);
